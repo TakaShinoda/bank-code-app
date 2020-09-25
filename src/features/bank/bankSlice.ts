b5bb7f9b14ee65804176c5275749a50f6e8ddf6b
Takahiro <tkhr_621@docomo.ne.jp>
2020-09-25 23:52:04 +0900

    スライス追加(途中経過)

---
+++ src/features/bank/bankSlice.ts
@@ -0,0 +1,6 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+// import { RootState } from '../../app/store'
+// import dataDaily from './apiDataDaily.json'
+
+const apiUrl = 'https://bankcode-api.appspot.com/api/bank/JP?name='
