bd2657902621f8f00c691e4518ee68a8e8f2a99b
Takahiro <tkhr_621@docomo.ne.jp>
2020-09-26 18:11:12 +0900

    feat: create store.ts

---
+++ src/app/store.ts
@@ -0,0 +1,16 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+//import bankReducer from '../features/bank/bankSlice'
+
+export const store = configureStore({
+  reducer: {
+    // bank: bankReducer,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
