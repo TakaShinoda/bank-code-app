e1e5d8ea26812109f73c79e7ca86b5e032452f1e
Takahiro <tkhr_621@docomo.ne.jp>
2020-10-03 17:28:30 +0900

    add initial state

--- src/features/bank/bankSlice.ts
+++ src/features/bank/bankSlice.ts
@@ -1,6 +1,18 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
-// import { RootState } from '../../app/store'
-// import dataDaily from './apiDataDaily.json'
+import { RootState } from '../../app/store'
+import dataBank from './apiDataBank.json'
 
 const apiUrl = 'https://bankcode-api.appspot.com/api/bank/JP?name='
+
+type DATABANK = typeof dataBank
+
+type bankState = {
+    bank: DATABANK
+    bankname: string
+}
+
+const initialState: bankState = {
+    bank: dataBank,
+    bankname: ''
+}
