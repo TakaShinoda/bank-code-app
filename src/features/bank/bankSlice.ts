d6e6fbdbac24feaf82780b270bdceba66495394c
Takahiro <tkhr_621@docomo.ne.jp>
2020-10-06 00:38:12 +0900

    とりあえず金融機関コードのスライス作成

--- src/features/bank/bankSlice.ts
+++ src/features/bank/bankSlice.ts
@@ -3,16 +3,44 @@ import axios from 'axios'
 import { RootState } from '../../app/store'
 import dataBank from './apiDataBank.json'
 
-const apiUrl = 'https://bankcode-api.appspot.com/api/bank/JP?name='
+const apiUrl = 'https://bankcode-api.appspot.com/api/bank'
 
 type DATABANK = typeof dataBank
 
 type bankState = {
-    bank: DATABANK
-    bankname: string
+  bank: DATABANK
+  bankname: string
 }
 
 const initialState: bankState = {
-    bank: dataBank,
-    bankname: ''
+  bank: dataBank,
+  bankname: '',
 }
+
+export const fetchAsyncGetBankCode = createAsyncThunk(
+  'bank/getBankCode',
+  async (bankName: string) => {
+    const { data } = await axios.get<DATABANK>(`${apiUrl}/JP?name=${bankName}`)
+    return { data: data, bankName: bankName }
+  }
+)
+
+const bankSlice = createSlice({
+  name: 'bank',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGetBankCode.fulfilled, (state, action) => {
+      return {
+        ...state,
+        data: action.payload.data,
+        bankName: action.payload.bankName,
+      }
+    })
+  },
+})
+
+export const selectBank = (state: RootState) => state.bank.data
+export const selectBankName = (state: RootState) => state.bank.bankName
+
+export default bankSlice.reducer
