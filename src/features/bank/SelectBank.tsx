abd394dc51c232f57f6442bea0fff266cb409a45
Takahiro <tkhr_621@docomo.ne.jp>
2020-10-08 00:00:25 +0900

    主要ボタンを作成

--- src/features/bank/SelectBank.tsx
+++ src/features/bank/SelectBank.tsx
@@ -2,11 +2,26 @@ import React, { FC } from 'react'
 import { Button } from 'semantic-ui-react'
 
 export const SelectBank: FC = () => {
+  const megaBank = [
+    '三菱',
+    'みずほ',
+    '三井住友',
+    'りそな',
+    '新生',
+    'あおぞら',
+    'ゆうちょ',
+  ]
   return (
     <div>
-      <Button inverted color='blue'>
+      {megaBank.map((bank, i) => (
+        <Button inverted color="blue" key={i}>
+          {bank}
+        </Button>
+      ))}
+
+      {/* <Button inverted color='blue'>
         Blue
-      </Button>
+      </Button> */}
     </div>
   )
 }
