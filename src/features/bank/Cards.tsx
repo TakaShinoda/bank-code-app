8aa5b137225689a9205a358edf51b086828bbacc
Takahiro <tkhr_621@docomo.ne.jp>
2020-10-18 21:22:57 +0900

    fx: namkname出力

--- src/features/bank/Cards.tsx
+++ src/features/bank/Cards.tsx
@@ -1,18 +1,21 @@
 import React, { FC } from 'react'
 import { Button, Card } from 'semantic-ui-react'
+import { useSelector } from 'react-redux'
+import { selectBank } from './bankSlice'
 
 export const Cards: FC = () => {
+  const bank = useSelector(selectBank)
   return (
     <div>
       <Card.Group>
         <Card>
           <Card.Content>
-            <Card.Header>Steve Sanders</Card.Header>
+            <Card.Header>{bank[0].hira}</Card.Header>
           </Card.Content>
           <Card.Content extra>
             <div className="ui two buttons">
               <Button inverted color="blue">
-                Approve
+                コード
               </Button>
             </div>
           </Card.Content>
